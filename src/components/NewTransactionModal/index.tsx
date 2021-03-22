d3662e6afb12124941d35a4a2af0c809975de321
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2021-03-21 21:25:39 -0300

    Created Modal Income and Outcome Buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -24,7 +26,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           <h2>Cadastar transação</h2>
 
           <input placeholder="Título" />
-          <input placeholder="Título" />
+
+          <input placeholder="Valor" />
+
+          <TransactionTypeContainer>
+            <button
+              type="button"
+            >
+              <img src={incomeImg} alt="Entrada"/>
+              <span>Entrada</span>
+            </button>
+
+            <button
+              type="button"
+            >
+              <img src={outcomeImg} alt="Saída"/>
+              <span>Saída</span>
+            </button>
+          </TransactionTypeContainer>
+
           <input placeholder="Categoria" type="number" />
 
           <button type="submit">Cadastrar</button>
