66c9633c7249ad27b5cf8c885d8b6931a07257ec
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2021-03-22 20:42:22 -0300

    Created Context & Transaction Hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -21,13 +22,13 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
